63651e7ef6a2ea4065ce5d7e33a42b1f4e358328
jarhkel <lekhrajvarshney@gmail.com>
2015-07-11 23:55:59 +0530

    links of css and javascript to be included in each file

---
+++ header.php
@@ -0,0 +1,10 @@
+<script src="js/jquery.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="js/checkbox.js"></script>
+<script src="js/Chart.js"></script>
+<script src="js/table-fixed-header.js"></script>
+<script src="js/legend.js"></script>
+<link rel="stylesheet" type="text/css" href="css/bootstrap-white.css">
+<link rel="stylesheet" type="text/css" href="css/checkbox-bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/table-fixed-header.css">
+<link rel="stylesheet" type="text/css" href="css/demo.css">
