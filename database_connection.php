656919e72170d09661a9145846af61d5671d8356
yashjain28 <yashkjain1994@gmail.com>
2015-07-11 23:38:32 +0530

    data base connection edited

--- database_connection.php
+++ database_connection.php
@@ -2,15 +2,20 @@
 
 DEFINE('DATABASE_USER', 'root');
 DEFINE('DATABASE_PASSWORD', 'code4good');
-DEFINE('DATABASE_HOST', 'ec2-52-69-94-146.ap-northeast-1.compute.amazonaws.com');
+DEFINE('DATABASE_HOST', 'localhost');
 DEFINE('DATABASE_NAME', 'impact');
-date_default_timezone_set('UTC');
+
 
 // Make the connection:
-$dbc = @mysqli_connect(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD,
+$dbc =mysql_connect(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD,
     DATABASE_NAME);
 if (!$dbc) {
-    trigger_error('Could not connect to MySQL: ' . mysqli_connect_error());
+    echo "<br>connection error<br>";
+}
+else {
+	echo "<br>data has been inserted<br>";
+	//$_session['username']=$uname;
+				//	header('Location: ../trial2.html');
 }
 
 ?>
