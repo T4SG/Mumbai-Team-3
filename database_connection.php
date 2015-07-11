2d5215334697ee467f18bc3c76ac1550996bc98a
jarhkel <lekhrajvarshney@gmail.com>
2015-07-11 18:31:19 +0530

    database connection

---
+++ database_connection.php
@@ -0,0 +1,29 @@
+<?php
+
+
+DEFINE('DATABASE_USER', 'user');
+DEFINE('DATABASE_PASSWORD', '');
+DEFINE('DATABASE_HOST', 'localhost');
+DEFINE('DATABASE_NAME', 'implementation');
+date_default_timezone_set('UTC');
+
+
+/*
+DEFINE('DATABASE_USER', 'root');
+DEFINE('DATABASE_PASSWORD', 'root');
+DEFINE('DATABASE_HOST', 'localhost');
+DEFINE('DATABASE_NAME', 'mdm_for_qc');
+date_default_timezone_set('UTC');
+*/
+
+
+
+// Make the connection:
+$dbc = @mysqli_connect(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD,
+    DATABASE_NAME);
+
+if (!$dbc) {
+    trigger_error('Could not connect to MySQL: ' . mysqli_connect_error());
+}
+
+?>
